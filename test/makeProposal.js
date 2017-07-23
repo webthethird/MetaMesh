af9ab43cc64688f050dd599aeec63ac5bb55b1b1
djudjuu <julfaber@gmail.com>
2017-07-22 20:43:48 -0400

    utils folder

--- test/makeProposal.js
+++ test/makeProposal.js
@@ -2,20 +2,16 @@ var ProposalRegistry = artifacts.require("ProposalRegistry.sol");
 var UserRegistry = artifacts.require("UserRegistry.sol");
 var Proposal = artifacts.require("Proposal.sol");
 
+utils = require("../js/utils.js")
+
 contract("ProposalRegistry", function(accounts) {
     describe("ProposalRegistry", async () => {
         var propReg;
-        var userReg;
         var proposal;
-        it("should  create a ", async () => {
-            propReg = await ProposalRegistry.deployed()
-            userReg = await UserRegistry.deployed()
-            userRegAddress = await propReg.userRegistry.call()
-            assert.equal(userRegAddress, userReg.address, "ProposalRegistry does not")
-        })
         it("should create a Proposal", async () => {
+            propReg = await ProposalRegistry.deployed()
             receiptFromMakeProposal = await propReg.makeProposal(10,10)
-            proposalAddress = getProposalAddress(receiptFromMakeProposal)
+            proposalAddress = utils.getProposalAddress(receiptFromMakeProposal)
             proposal = await Proposal.at(proposalAddress)
             // console.log(proposalAddress)
             exists = await propReg.proposalExists.call(proposalAddress)
@@ -24,8 +20,8 @@ contract("ProposalRegistry", function(accounts) {
     })
 })
 
-function getProposalAddress(receiptFromMakeProposal){
-    tmp = receiptFromMakeProposal.logs[0].args.proposal
-    // console.log(tmp)
-    return tmp
-}
+// function getProposalAddress(receiptFromMakeProposal){
+//     tmp = receiptFromMakeProposal.logs[0].args.proposal
+//     // console.log(tmp)
+//     return tmp
+// }
